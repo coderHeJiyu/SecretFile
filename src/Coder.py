faf4b1d36cccf5358a531969d4f92694e6be1e69
kaoxing <3032738029@qq.com>
2023-01-28 14:42:01 +0800

    加解密器封装和修改

--- src/Coder.py
+++ src/Coder.py
@@ -3,86 +3,115 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import base64
-
-block_size = 128
-
-
-def aes_encrypt(secret_key, data):
-    """加密数据
-    :param secret_key: 加密秘钥
-    :param data: 需要加密数据
-    """
-    # 将数据转换为byte类型
-    # data = data.encode("utf-8")
-    secret_key = secret_key.encode("utf-8")
-
-    # 填充数据采用pkcs7
-    padder = padding.PKCS7(block_size).padder()
-    pad_data = padder.update(data) + padder.finalize()
-
-    # 创建密码器
-    cipher = Cipher(
-        algorithms.AES(secret_key),
-        mode=modes.ECB(),
-        backend=default_backend()
-    )
-    # 加密数据
-    encryptor = cipher.encryptor()
-    encrypted_data = encryptor.update(pad_data)
-    return base64.b64encode(encrypted_data)
+from abc import abstractmethod
 
 
-def aes_decrypt(secret_key, data):
-    """解密数据
-    """
-    secret_key = secret_key.encode("utf-8")
-    data = base64.b64decode(data)
+class Coder:
+    def __init__(self):
+        """
+        加解密器，self.prpgress是当前进度，是一个从0-100的数字
+        """
+        self.progress = 0
+        self.__block_size = 128
 
-    # 创建密码器
-    cipher = Cipher(
-        algorithms.AES(secret_key),
-        mode=modes.ECB(),
-        backend=default_backend()
-    )
-    decryptor = cipher.decryptor()
-    decrypt_data = decryptor.update(data)
-    unpadder = padding.PKCS7(block_size).unpadder()
-    unpad_decrypt_data = unpadder.update(decrypt_data) + unpadder.finalize()
-    return unpad_decrypt_data
+    def __aes_encrypt(self, secret_key, data):
+        """加密数据
+        :param secret_key: 加密秘钥
+        :param data: 需要加密数据
+        """
+        # 将数据转换为byte类型
+        # data = data.encode("utf-8")
+        secret_key = secret_key.encode("utf-8")
 
+        # 填充数据采用pkcs7
+        padder = padding.PKCS7(self.__block_size).padder()
+        pad_data = padder.update(data) + padder.finalize()
 
-# 加密
-def encode(source, save, key):
-    key = adjust(key)
-    file1 = open(source, 'rb')
-    file2 = open(save, 'wb')
-    for line in file1:
-        temp = aes_encrypt(key, line)
-        file2.write(temp + "\n".encode("utf-8"))
-    file1.close()
-    file2.close()
+        # 创建密码器
+        cipher = Cipher(
+            algorithms.AES(secret_key),
+            mode=modes.ECB(),
+            backend=default_backend()
+        )
+        # 加密数据
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(pad_data)
+        return base64.b64encode(encrypted_data)
 
+    def __aes_decrypt(self, secret_key, data):
+        """解密数据
+        """
+        secret_key = secret_key.encode("utf-8")
+        data = base64.b64decode(data)
 
-# 解密
-def decode(save, result, key):
-    key = adjust(key)
-    file1 = open(save, 'rb')
-    file2 = open(result, 'wb')
-    try:
-        for line in file1:
-            temp = aes_decrypt(key, line)
-            file2.write(temp)
-    except ValueError:
+        # 创建密码器
+        cipher = Cipher(
+            algorithms.AES(secret_key),
+            mode=modes.ECB(),
+            backend=default_backend()
+        )
+        decryptor = cipher.decryptor()
+        decrypt_data = decryptor.update(data)
+        unpadder = padding.PKCS7(self.__block_size).unpadder()
+        unpad_decrypt_data = unpadder.update(decrypt_data) + unpadder.finalize()
+        return unpad_decrypt_data
+
+    # 加密
+    def encode(self, source, save, key):
+        """
+        加密，参数为(待加密文件路径，结果路径，密钥)
+        """
+        key = self.__adjust(key)
+        file1 = open(source, 'rb')
+        file2 = open(save, 'wb')
+        lines = file1.readlines()
+        length = len(lines)
+        for i, line in enumerate(lines):
+            temp = self.__aes_encrypt(key, line)
+            file2.write(temp + "\n".encode("utf-8"))
+            self.progress = i * 100 / length
+            # print(self.progress)
         file1.close()
         file2.close()
-        os.remove(result)
-        return False
-    else:
-        file1.close()
-        file2.close()
-        return True
+
+    # 解密
+    def decode(self, save, result, key):
+        """
+        解密，参数为(待解密文件路径，结果路径，密钥)
+        """
+        key = self.__adjust(key)
+        file1 = open(save, 'rb')
+        file2 = open(result, 'wb')
+        try:
+            lines = file1.readlines()
+            length = len(lines)
+            for i, line in enumerate(lines):
+                temp = self.__aes_decrypt(key, line)
+                file2.write(temp)
+                self.progress = i * 100 / length
+                # print(self.progress)
+        except ValueError:
+            print(ValueError)
+            file1.close()
+            file2.close()
+            os.remove(result)
+            return False
+        else:
+            file1.close()
+            file2.close()
+            return True
+
+    def __adjust(self, key):
+        n = 16 - len(key)
+        return key + n * "$"
+
+    @abstractmethod
+    def progress_change(self):
+        pass
 
 
-def adjust(key):
-    n = 16 - len(key)
-    return key + n * "$"
+if __name__ == '__main__':
+    coder = Coder()
+    file1 = "../password = 666.hlx"
+    file2 = "../password = 666.mp4"
+    coder.decode(file1, file2, "666")
